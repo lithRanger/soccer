155046634e6af379b846b4fedb61bdaf4a99aedc
lithRanger <xx5683968@qq.com>
2019-06-16 16:00:40 +0800

    '初始化上传';

---
+++ application/extra/addons.php
@@ -0,0 +1,23 @@
+<?php
+
+return array (
+  'autoload' => false,
+  'hooks' => 
+  array (
+    'app_init' => 
+    array (
+      0 => 'epay',
+    ),
+    'admin_login_init' => 
+    array (
+      0 => 'loginbg',
+    ),
+  ),
+  'route' => 
+  array (
+    '/example$' => 'example/index/index',
+    '/example/d/[:name]' => 'example/demo/index',
+    '/example/d1/[:name]' => 'example/demo/demo1',
+    '/example/d2/[:name]' => 'example/demo/demo2',
+  ),
+);
